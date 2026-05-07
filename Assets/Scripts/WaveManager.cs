4e96d0b4c635b7b2f4bc0eaaa84038acf845ad43
genaro-cravero <nicocraveroo@gmail.com>
2026-05-07 15:27:34 -0300

    CREATED: WaveManager

---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,82 @@
+using Health;
+using System.Collections;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [Header("Wave")]
+    [SerializeField] private Wave[] _waves;
+    [SerializeField] private int _currentWave = 0;
+
+    [Header("Spawning")]
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private GameObject _meleeEnemyPrefab;
+    [SerializeField] private GameObject _rangedEnemyPrefab;
+
+    [Header("Timing")]
+    [SerializeField] private float _timeBetweenWaves = 3f;
+    [SerializeField] private float _spawnDelay = 0.5f;
+
+    private int _enemiesAlive;
+
+    private void Start()
+    {
+        StartCoroutine(StartWave());
+    }
+
+    private IEnumerator StartWave()
+    {
+        yield return new WaitForSeconds(_timeBetweenWaves);
+        if(_currentWave >=  _waves.Length)
+        {
+            //ToDo Winning condition
+            yield break;
+        }
+        var currentWave = _waves[_currentWave];
+        int meleeCount = currentWave.meleeCount;
+        int rangedCount = currentWave.rangedCount;
+
+        _enemiesAlive = meleeCount + rangedCount;
+
+        for (int i = 0; i < meleeCount; i++)
+        {
+            SpawnEnemy(_meleeEnemyPrefab);
+            yield return new WaitForSeconds(_spawnDelay);
+        }
+
+        for (int i = 0; i < rangedCount; i++)
+        {
+            SpawnEnemy(_rangedEnemyPrefab);
+            yield return new WaitForSeconds(_spawnDelay);
+        }
+    }
+
+    private void SpawnEnemy(GameObject enemyPrefab)
+    {
+        Transform randomSpawn = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+
+        GameObject enemy = Instantiate(enemyPrefab, randomSpawn.position, Quaternion.identity);
+
+        HealthComponent enemyHealth = enemy.GetComponent<HealthComponent>();
+
+        enemyHealth.OnDie += HandleEnemyDeath;
+    }
+
+    private void HandleEnemyDeath()
+    {
+        _enemiesAlive--;
+
+        if (_enemiesAlive <= 0)
+        {
+            _currentWave++;
+            StartCoroutine(StartWave());
+        }
+    }
+}
+
+[System.Serializable]
+public struct Wave
+{
+    public int meleeCount;
+    public int rangedCount;
+}
