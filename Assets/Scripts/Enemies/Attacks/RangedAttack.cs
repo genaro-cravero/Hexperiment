de5987473467552702501cf4b02157edddea907f
genaro-cravero <nicocraveroo@gmail.com>
2026-05-08 16:59:42 -0300

    ADDED: Ranged enemy predict player pos before shooting

--- Assets/Scripts/Enemies/Attacks/RangedAttack.cs
+++ Assets/Scripts/Enemies/Attacks/RangedAttack.cs
@@ -1,3 +1,4 @@
+using Player;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,18 +12,24 @@ namespace Enemy
         [SerializeField] private Bullet _bulletPrefab;
         [SerializeField] private Transform _shootPoint;
         [SerializeField] private LayerMask _shootLayer;
+
+        [SerializeField] private BulletData _bulletData;
         private IObjectPool<Bullet> _bulletPool;
 
         private float _damage = 1f;
         private float _fireRate = 1f;
         private float _lastAttackTime;
-        private const float ROT_THRESHOLD = 0.3f;
+        private const float ROT_THRESHOLD = 0.15f;
 
         private bool _initialized;
         private bool _isAttacking;
         public bool IsAttacking => _isAttacking;
         private Transform _player;
         private CharacterData _data;
+        private Vector3 _lastPlayerPosition;
+        private Vector3 _playerVelocity;
+        private float _lastVelocityTime;
+        private float _predictionAccuracy = 0.75f; //ToDo Update with every wave or make more accurate enemies
 
         public void Initialize(EnemyContext context)
         {
@@ -30,6 +37,8 @@ namespace Enemy
             _damage = context.enemyData.attackDamage;
             _player = context.player;
             _data = context.enemyData;
+            _lastPlayerPosition = _player.position;
+            _lastVelocityTime = Time.time;
             _bulletPool = new ObjectPool<Bullet>(
                 createFunc: () => Instantiate(_bulletPrefab),
                 actionOnGet: bullet =>
@@ -57,7 +66,7 @@ namespace Enemy
         {
             while(Time.time < _lastAttackTime + _fireRate)
             {
-                yield return RotateToPlayer();
+                yield return RotateToTarget();
             }
             var bullet = _bulletPool.Get();
             bullet.transform.SetPositionAndRotation(_shootPoint.position, _shootPoint.rotation);
@@ -68,15 +77,15 @@ namespace Enemy
             _lastAttackTime = Time.time;
         }
 
-        private IEnumerator RotateToPlayer()
+        private IEnumerator RotateToTarget()
         {
-            Vector3 targetDirection = _player.position - transform.position;
+            Vector3 targetDirection = CalculateTargetDir() - transform.position;
             targetDirection.y = 0f;
             Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
 
             while (Quaternion.Angle(transform.rotation, targetRotation) > ROT_THRESHOLD)
             {
-                targetDirection = _player.position - transform.position;
+                targetDirection = CalculateTargetDir() - transform.position;
                 targetDirection.y = 0f;
                 targetRotation = Quaternion.LookRotation(targetDirection);
 
@@ -84,6 +93,33 @@ namespace Enemy
                     targetRotation, _data.moveSpeed);
                 yield return null;
             }
+        }
+
+        private Vector3 CalculateTargetDir()
+        {
+            UpdatePlayerVelocity();
+
+            Vector3 target = _player.position;
+            float bulletSpeed = Mathf.Max(0.01f, _bulletData.speed);
+            float distToPlayer = Vector3.Distance(_player.position, transform.position);
+            float timeToTarget = distToPlayer / bulletSpeed;
+
+            float accuracy = Mathf.Clamp01(_predictionAccuracy);
+            target += _playerVelocity * timeToTarget * accuracy;
+            target.y = 0f;
+            return target;
+        }
+
+        private void UpdatePlayerVelocity()
+        {
+            float deltaTime = Time.time - _lastVelocityTime;
+            if (deltaTime <= 0f)
+                return;
+
+            Vector3 currentPosition = _player.position;
+            _playerVelocity = (currentPosition - _lastPlayerPosition) / deltaTime;
+            _lastPlayerPosition = currentPosition;
+            _lastVelocityTime = Time.time;
         }
     }
 }
